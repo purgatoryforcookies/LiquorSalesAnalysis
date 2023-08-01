9fa4d591db9dca599ab70eb99e39b3da34082e36
Max Caroe <max.caroe@gmail.com>
2023-08-01 21:40:18 +0300

    age modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css'
 import ViewLayout from './layouts/ViewLayout'
 import Home from './layouts/home/Home'
 import Search from './layouts/search/Search'
 import Nav from './components/nav/Nav'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import useLocalStorage from './service/hooks/useLocalStorage'
+import AgeModal from './components/ageModal/AgeModal'
 
 
 const queryClient = new QueryClient({
@@ -20,11 +21,20 @@ const queryClient = new QueryClient({
 
 function App() {
 
+  const [isAdult, setIsAdult] = useLocalStorage('isOfAge', false)
+
+  if (!isAdult) {
+    return (
+      <AgeModal handle={setIsAdult}/>
+    )
+  }
+
+
+
   return (
     <div>
       <Nav />
       <ViewLayout 
-      // height="auto"
        color='#F3F2F2'>
         <Home />
       </ViewLayout>
