14e80a4365f8113aa097b9083a6d575b4074d0fe
Max Caroe <max.caroe@gmail.com>
2024-05-26 12:17:39 +0300

    sw registration fiddling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register("cacheWorker.js", {
-      type: "module",
-    })
-    .then((registration) => {
-      console.log("Service Worker registered with scope:", registration.scope);
-    })
-    .catch((error) => {
-      console.log("Service Worker registration failed:", error);
-    });
-}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
