8ea2ae836a371ce1e32325782400dc82d0587e0d
Max Caroe <max.caroe@gmail.com>
2024-05-29 22:59:22 +0300

    change api address

--- client/src/service/liquorApi.tsx
+++ client/src/service/liquorApi.tsx
@@ -3,7 +3,7 @@ import { EngineSchema, LiquorSchema } from "./types";
 import { QueryFunctionContext } from "react-query";
 import { ZodError } from "zod";
 
-const URL = "http://localhost:3002/elapi"
+const URL = "https://purgatoryforcookies.com/elapi"
 
 
 const getSearchResults = async ({ queryKey }: QueryFunctionContext) => {
